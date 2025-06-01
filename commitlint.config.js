206a76df430a5b3d09d78a59db754b20875328f1
TahaManaquib <taha.manaquib53@gmail.com>
2025-06-01 20:29:30 +0530

    feature: Commitlint setup

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ["@commitlint/cli", "@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feature",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+      ],
+    ],
+    "subject-case": [2, "always", "sentence-case"],
+  },
+};
